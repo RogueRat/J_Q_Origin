78d2a005ffa2cd3565a87108c97a741a3c186f33
Emily Stage <birgitte_stage@yahoo.dk>
2022-10-25 17:24:45 +0200

    New

--- src/job_queue.c
+++ src/job_queue.c
@@ -16,14 +16,15 @@ int job_queue_init(struct job_queue *job_queue, int capacity) {
 }
 
 int job_queue_destroy(struct job_queue *job_queue) {
-  if (Empty(job_queue)){
+  if (job_queue->size == 0){
     free(job_queue);
   }
 }
 
 int job_queue_push(struct job_queue *job_queue, void *data) {
   while(job_queue->size >= job_queue->capacity);
-  job_queue->start + (job_queue->back * sizeof(void*)) = data;
+  void* vp = job_queue->start + (job_queue->back * sizeof(void*));
+  vp = data;
   job_queue->back++;
   if(job_queue->back >= job_queue->capacity) {
     job_queue->back = 0;
@@ -31,19 +32,27 @@ int job_queue_push(struct job_queue *job_queue, void *data) {
 }
 
 int job_queue_pop(struct job_queue *job_queue, void **data) {
-  /*if (Empty(job_queue)){
+  
+  while (job_queue->size == 0){
   }
-  else if(job_queue -> index findes)
-  {
-    remove element
-  }
-  else if (job_queue_destroy(job_queue))
+  if (job_queue->size == NULL)
   {
     return -1;
   }
+  void* vp = job_queue->start + (job_queue->front * sizeof(void*));
+  if((int)*vp <= 0 || *vp > 0)
+  {
+    data = &job_queue->start +(job_queue->front * sizeof(void*));
+    vp = job_queue->start + (job_queue->back * sizeof(void*)); 
+    vp = NULL; 
+    job_queue->front++;
+    if (job_queue->front >= job_queue->capacity) {
+      job_queue->front = 0; 
+    }
+    job_queue->size--;  
+    return 0;
+  }
   else{
     return 1;
-  }*/
-  
-  
+  }
 }
