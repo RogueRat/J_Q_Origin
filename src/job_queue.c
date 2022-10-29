a5553b9e1498ad503db93d21761415823ce949fb
RogueRat <birklaus@gmail.com>
2022-10-29 12:13:11 +0200

    New Queue

--- src/job_queue.c
+++ src/job_queue.c
@@ -4,6 +4,9 @@
 
 #include "job_queue.h"
 
+pthread_mutex_t stdout_mutex2 = PTHREAD_MUTEX_INITIALIZER;
+
+
 int job_queue_init(struct job_queue *job_queue, int capacity) {
   job_queue->start = malloc(capacity * sizeof(void*));
   if(job_queue->start == NULL) return -1;
@@ -40,8 +43,11 @@ int job_queue_push(struct job_queue *job_queue, void *data) {
 }
 
 int job_queue_pop(struct job_queue *job_queue, void **data) {
-  if(job_queue->start == NULL) return -1; 
-  while (job_queue->size <= 0){} 					// Block until queue has content
+  pthread_mutex_lock(&stdout_mutex2);
+  if(job_queue->start == NULL) return -1;
+  while (job_queue->size <= 0 ){}					// Block until queue has content
+   				
+  //printf("%d\n", job_queue->front);				
   *data = job_queue->start[job_queue->front];							
   if(*data != job_queue->start[job_queue->front]) return -2;		// Check that the copy worked before we update the queue
   
@@ -50,6 +56,7 @@ int job_queue_pop(struct job_queue *job_queue, void **data) {
     job_queue->front = 0;
   }
   
-  job_queue->size--;	
+  job_queue->size--;
+  pthread_mutex_unlock(&stdout_mutex2);
   return 0;
 }
